2de8298a10438e6d430eaf072d84853e4f74304f
Your Name <atuly152001@gmail.com>
2021-05-08 16:38:10 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Route, Router, Switch } from "react-router";
 import './App.css';
 import Header from "./Header";
 import Main from "./Main";
@@ -7,6 +8,19 @@ import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="App ">
+       
+       <Router>
+         <Switch>
+           <Route path="/" >
+
+           </Route>
+           <Route path="/">
+
+
+           </Route>
+         </Switch>
+       </Router>
+           
       <Header />
       <div className="flex">
         <Sidebar  />
